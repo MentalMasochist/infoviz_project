538dffdd9f2882cb5fe9343cc442ff3a6f2b04f6
Richard Brooks <richardbrks@gmail.com>
2014-11-07 10:25:46 -0500

    database connection moved and drop tables added

--- website/build/create_tables.php
+++ website/build/create_tables.php
@@ -2,14 +2,26 @@
 $dbhost = 'localhost';
 $dbuser = 'root';
 $dbpass = '';
+$database = 'arXiv_db';
 
 // connect to database
-$conn = mysql_connect($dbhost, $dbuser, $dbpass);
-if(! $conn )
+$server = mysql_connect($dbhost, $dbuser, $dbpass);
+$conn = mysql_select_db($database, $server);
+
+if(! $server )
 {
   die('Could not connect: ' . mysql_error());
 }
-echo 'Connected successfully<br />';
+echo ' - connected successfully<br />';
+
+// drop papers table
+$sql = "DROP TABLE IF EXISTS arXiv_db.papers";
+$retval = mysql_query( $sql );
+if(! $retval )
+{
+  die('Could not create table: ' . mysql_error());
+}
+echo " - papers table dropped successfully <br />";
 
 // create papers table
 $sql = "CREATE TABLE IF NOT EXISTS arXiv_db.papers( ".
@@ -19,12 +31,21 @@ $sql = "CREATE TABLE IF NOT EXISTS arXiv_db.papers( ".
        "set_spec VARCHAR(40) NOT NULL, ".
        "description TEXT NOT NULL, ".
        "PRIMARY KEY ( paper_id, set_spec )); ";
-$retval = mysql_query( $sql, $conn );
+$retval = mysql_query( $sql );
 if(! $retval )
 {
   die('Could not create table: ' . mysql_error());
 }
-echo "Papers table created successfully <br />";
+echo " - papers table created successfully <br />";
+
+// drop authors table
+$sql = "DROP TABLE IF EXISTS arXiv_db.authors";
+$retval = mysql_query( $sql );
+if(! $retval )
+{
+  die('Could not create table: ' . mysql_error());
+}
+echo " - authors table dropped successfully <br />";
 
 // create authors table
 $sql = "CREATE TABLE IF NOT EXISTS arXiv_db.authors( ".
@@ -32,12 +53,21 @@ $sql = "CREATE TABLE IF NOT EXISTS arXiv_db.authors( ".
        "set_spec VARCHAR(40) NOT NULL, ".
        "author_name VARCHAR(500) NOT NULL, ".
        "PRIMARY KEY ( paper_id, set_spec, author_name )); ";
-$retval = mysql_query( $sql, $conn );
+$retval = mysql_query( $sql );
 if(! $retval )
 {
   die('Could not create table: ' . mysql_error());
 }
-echo "Authors table created successfully <br />";
+echo " - authors table created successfully <br />";
+
+// drop sujects table
+$sql = "DROP TABLE IF EXISTS arXiv_db.subjects";
+$retval = mysql_query( $sql );
+if(! $retval )
+{
+  die('Could not create table: ' . mysql_error());
+}
+echo " - subjects table dropped successfully <br />";
 
 // create subjects table
 $sql = "CREATE TABLE IF NOT EXISTS arXiv_db.subjects( ".
@@ -46,11 +76,11 @@ $sql = "CREATE TABLE IF NOT EXISTS arXiv_db.subjects( ".
        "subject_name VARCHAR(500) NOT NULL, ".
        "PRIMARY KEY ( paper_id, set_spec, subject_name )); ";
 
-$retval = mysql_query( $sql, $conn );
+$retval = mysql_query( $sql );
 if(! $retval )
 {
   die('Could not create table: ' . mysql_error());
 }
-echo "Subjects table created successfully <br />";
-mysql_close($conn);
+echo " - subjects table created successfully <br />";
+mysql_close( $server );
 ?>
