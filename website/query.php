3c615d6eeafc71b62ca530e938c6ec8923cf7916
Richard Brooks <richardbrks@gmail.com>
2014-11-08 14:14:15 -0500

    returns query in json (query is test only, not complete)

--- website/query.php
+++ website/query.php
@@ -3,12 +3,46 @@
 
 <?php
 
-print_r($_POST);
+function f_mysql_query($query) {
+	$dbhost = 'localhost';
+	$dbuser = 'root';
+	$dbpass = 'e5ye5ye5y';
+	$database = 'arXiv_db';
+
+	// connect to database
+	$server = mysql_connect($dbhost, $dbuser, $dbpass);
+	$conn = mysql_select_db($database, $server);
+
+	if(! $conn )
+	{
+	  die('Could not connect: ' . mysql_error());
+	}
+
+	$retval = mysql_query( $query );
+
+	if(! $retval )
+	{
+	  die('Could not load into table: ' . mysql_error());
+	}
+
+	$data = array();
+    
+    for ($x = 0; $x < mysql_num_rows($retval); $x++) {
+        $data[] = mysql_fetch_assoc($retval);
+    }
+    
+    echo json_encode($data);  
+
+	mysql_close($conn);
+}
+
+// print_r($_POST);
 
 // define variables
 $form_subjects = $_POST["subjects"];
 $form_authors = $_POST["authors"];
 $form_keywords = $_POST["keywords"];
+$query_papers = "";
 
 // check if inputs exist
 if (empty($form_subjects)) {
@@ -31,18 +65,22 @@ if (empty($form_keywords)) {
 	$active_keywords = TRUE;
 	$arr_keywords = explode(",", $form_keywords);
 	foreach ($arr_keywords as &$value) {
-		$value = '+'.trim($value);
-	}
-	foreach ($arr_keywords as &$value) {
-		echo $value.'<br>';
+		$query_papers = $query_papers.' +'.trim($value);
 	}
 }
 
-/////////////////////////////////////////
-// trim data
-// check for spaces
-// check for phrases
-//
+//////////////////////
+// create full queries
+//////////////////////
+
+
+$query_papers = " SELECT paper_id, dt_created FROM papers                  ".
+                "     WHERE MATCH (title, description)                     ".
+                "     AGAINST ('".trim($query_papers)."' IN BOOLEAN MODE); ";
+
+// perform query
+f_mysql_query($query_papers);
+
 ?>
 
 </body>
