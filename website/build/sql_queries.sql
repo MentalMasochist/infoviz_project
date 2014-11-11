28b13972d2d42e91c1ef26625ddd0c58c1d80284
Richard Brooks <richardbrks@gmail.com>
2014-11-10 19:21:27 -0500

    sql cleaned and sanity checks added

--- website/build/sql_queries.sql
+++ website/build/sql_queries.sql
@@ -1,9 +1,16 @@
--- Notes:
-    -- when doing full text search, use boolean mode, 
-    --   or it will only return at max 50% of the entire corpus
+------------------------------
+-- NOTES ---------------------
+------------------------------
 
--- log in mysql
-mysql -uroot -pe5ye5ye5y
+-- - when doing full text search, use boolean mode, or it 
+--     will only return at max 50% of the entire corpus
+-- 
+-- 
+-- 
+
+------------------------------
+-- DATABASE SET UP -----------
+------------------------------
 
 -- log on with the permission to be able to load in
 mysql -uroot --local-infile=1 -p
@@ -11,10 +18,10 @@ mysql -uroot --local-infile=1 -p
 -- create database
 CREATE DATABASE IF NOT EXISTS arXiv_db;
 
+-- use db
 USE arXiv_db;
 
 -- papers table
-
 DROP TABLE IF EXISTS papers;
 
 CREATE TABLE IF NOT EXISTS papers (
@@ -32,7 +39,6 @@ ALTER TABLE papers
     ADD FULLTEXT INDEX title_desc
     (title, description);
 
--- LOAD DATA LOCAL INFILE 'papers_test.csv'
 LOAD DATA LOCAL INFILE 'papers.csv'
     INTO TABLE papers
     FIELDS TERMINATED BY '\t'
@@ -40,7 +46,6 @@ LOAD DATA LOCAL INFILE 'papers.csv'
     (paper_id, title, dt_created, set_spec, description); 
 
 -- authors table
-
 DROP TABLE IF EXISTS authors;
 
 CREATE TABLE IF NOT EXISTS authors (
@@ -54,7 +59,6 @@ CREATE TABLE IF NOT EXISTS authors (
 ALTER TABLE authors
     ADD FULLTEXT (author_name);
 
--- LOAD DATA LOCAL INFILE 'authors_test.csv'
 LOAD DATA LOCAL INFILE 'proper_authors.csv'
     INTO TABLE authors
     FIELDS TERMINATED BY '\t'
@@ -75,7 +79,6 @@ CREATE TABLE IF NOT EXISTS subjects (
 ALTER TABLE subjects
     ADD FULLTEXT (subject_name);
 
--- LOAD DATA LOCAL INFILE 'subjects_test.csv'
 LOAD DATA LOCAL INFILE 'proper_subjects.csv'
     INTO TABLE subjects
     FIELDS TERMINATED BY '\t'
@@ -83,18 +86,18 @@ LOAD DATA LOCAL INFILE 'proper_subjects.csv'
     (paper_id, set_spec, subject_name); 
 
 
-------------------------
--- TABLE DESCRIPTIONS --
-------------------------
+------------------------------
+-- TABLE DESCRIPTIONS --------
+------------------------------
 
 -- mysql> describe papers;
 -- +-------------+--------------+------+-----+---------+-------+
 -- | Field       | Type         | Null | Key | Default | Extra |
 -- +-------------+--------------+------+-----+---------+-------+
--- | paper_id    | varchar(100) | NO   | PRI | NULL    |       |
--- | title       | text         | NO   |     | NULL    |       |
+-- | paper_id    | varchar(150) | NO   | PRI | NULL    |       |
+-- | title       | text         | NO   | MUL | NULL    |       |
 -- | dt_created  | date         | NO   |     | NULL    |       |
--- | set_spec    | varchar(100) | NO   |     | NULL    |       |
+-- | set_spec    | varchar(40)  | NO   |     | NULL    |       |
 -- | description | text         | NO   |     | NULL    |       |
 -- +-------------+--------------+------+-----+---------+-------+
 
@@ -103,44 +106,38 @@ LOAD DATA LOCAL INFILE 'proper_subjects.csv'
 -- +-------------+--------------+------+-----+---------+-------+
 -- | Field       | Type         | Null | Key | Default | Extra |
 -- +-------------+--------------+------+-----+---------+-------+
--- | paper_id    | varchar(100) | NO   | PRI | NULL    |       |
--- | set_spec    | varchar(100) | NO   |     | NULL    |       |
+-- | paper_id    | varchar(150) | NO   | PRI | NULL    |       |
+-- | set_spec    | varchar(40)  | NO   |     | NULL    |       |
 -- | author_name | varchar(100) | NO   | PRI | NULL    |       |
 -- +-------------+--------------+------+-----+---------+-------+
--- 3 rows in set (0.00 sec)
 
 
 -- mysql> describe subjects;
 -- +--------------+--------------+------+-----+---------+-------+
 -- | Field        | Type         | Null | Key | Default | Extra |
 -- +--------------+--------------+------+-----+---------+-------+
--- | paper_id     | varchar(100) | NO   | PRI | NULL    |       |
--- | set_spec     | varchar(100) | NO   |     | NULL    |       |
+-- | paper_id     | varchar(150) | NO   | PRI | NULL    |       |
+-- | set_spec     | varchar(40)  | NO   |     | NULL    |       |
 -- | subject_name | varchar(100) | NO   | PRI | NULL    |       |
 -- +--------------+--------------+------+-----+---------+-------+
 
-----------------
--- sanity checks
-----------------
+------------------------------
+-- SANITY CHECKS -------------
+------------------------------
 
 -- all tables should have the same number of distinct paper ids
----------------------------------------------------------------
+SELECT COUNT(paper_id)
+FROM papers;
 
--- check papers
-SELECT COUNT(DISTINCT paper_id)
-    FROM papers;
+SELECT COUNT(paper_id)
+FROM authors;
 
--- check authors
-SELECT COUNT(DISTINCT paper_id)
-    FROM authors;
+SELECT COUNT(paper_id)
+FROM keywords;
 
--- check subjects
-SELECT COUNT(DISTINCT paper_id)
-    FROM subjects;
-
-
--- making queries
------------------
+--------------------
+-- making queries --
+--------------------
 
 -- idea: 
 -- query each table given whether if each form is filled out
@@ -151,67 +148,55 @@ SELECT COUNT(DISTINCT paper_id)
 -- - then inner join them to return the proper relation
 
 
--- Query 1
+------------------------------
+-- TEST QUERIES --------------
+------------------------------
+
+--
 SELECT dt_created, count(dt_created) FROM papers
     WHERE MATCH (title, description) 
     AGAINST ('+tomorrow' IN BOOLEAN MODE)
     GROUP BY dt_created;
 
--- Query 2
-    SELECT paper_id FROM authors
-        WHERE CONTAINS (author_name, '"roughgarden*"');
+--
+SELECT paper_id FROM authors
+    WHERE CONTAINS (author_name, '"roughgarden*"');
 
-    SELECT COUNT(*), paper_id, set_spec FROM authors
-        WHERE MATCH (author_name) AGAINST ('roughgarden' IN BOOLEAN MODE)
-        GROUP BY paper_id;
+--
+SELECT COUNT(*), paper_id, set_spec FROM authors
+    WHERE MATCH (author_name) AGAINST ('roughgarden' IN BOOLEAN MODE)
+    GROUP BY paper_id;
 
---------------------- 
--- inner join methods
----------------------
--- doesn't deal with multiple authors and subjects well (will give the union of )
+------------------------------
+-- QUERIES USED --------------
+------------------------------
 
+-- TEMPORARY TABLE METHOD
+-------------------------
 
-SELECT s.paper_id, p.paper_id, s.paper_id
-    FROM papers p
-    INNER JOIN authors a
-        ON p.paper_id = a.paper_id
-    INNER JOIN subjects s
-        ON p.paper_id = s.paper_id
-    WHERE MATCH (a.author_name) AGAINST ('trimm' IN BOOLEAN MODE);
+--
+-- creating temporary tables
+--
 
-SELECT s.subject_name
-    FROM papers p
-    INNER JOIN authors a
-        ON p.paper_id = a.paper_id
-    INNER JOIN subjects s
-        ON p.paper_id = s.paper_id
-    WHERE MATCH (p.title, p.description) AGAINST ('+algorithm' IN BOOLEAN MODE)
-    AND MATCH (a.author_name) AGAINST ('roughgarden' IN BOOLEAN MODE)
-    AND MATCH (s.subject_name) AGAINST ('*theory*' IN BOOLEAN MODE);
-
----------------------------
--- three temp tables method 
----------------------------
-
--- papers
+-- papers temp table
 DROP TABLE IF EXISTS temp_papers;
 CREATE TEMPORARY TABLE temp_papers AS (
     SELECT p.paper_id
         FROM papers p
-        WHERE MATCH (p.title, p.description) AGAINST ('+"Seiberg-Witten"' IN BOOLEAN MODE)
+        WHERE MATCH (p.title, p.description) AGAINST ('spin' IN BOOLEAN MODE)
 );
 
--- authors
+-- authors temp table
 DROP TABLE IF EXISTS temp_authors;
 CREATE TEMPORARY TABLE temp_authors AS (
 SELECT a.paper_id
     FROM authors a
-    WHERE MATCH (a.author_name) AGAINST ('trimm' IN BOOLEAN MODE)
+    WHERE MATCH (a.author_name) AGAINST ('Chacaltana Distler Trimm' IN BOOLEAN MODE)
     GROUP BY a.paper_id
-    HAVING count(a.paper_id) = 1
+    HAVING count(a.paper_id) = 3
 );
 
--- subjects 
+-- subjects table
 DROP TABLE IF EXISTS temp_subjects;
 CREATE TEMPORARY TABLE temp_subjects AS (
 SELECT s.paper_id
@@ -221,105 +206,132 @@ SELECT s.paper_id
     HAVING count(s.paper_id) = 1
 );
 
+-- 
+-- combine into temporary table
+-- 
+
+-- combine temporary tables into an active table set
 CREATE TEMPORARY TABLE active_papers AS (
 SELECT tp.paper_id
     FROM temp_papers tp
     INNER JOIN temp_authors ta 
     ON ta.paper_id = tp.paper_id
     INNER JOIN temp_subjects ts 
-    ON ts.paper_id = tp.paper_id;
+    ON ts.paper_id = tp.paper_id
 );
 
 
-SELECT count(paper_id) AS count_paper, 
-       year(dt_created) AS yr, 
-       month(dt_created) AS mn 
-       FROM papers 
-       WHERE paper_id IN(
-            SELECT paper_id 
-                 FROM active_papers 
-        ) 
-        GROUP BY year(dt_created), month(dt_created);
+-- 
+-- visualization specific queries
+-- 
+
+-- TREND GRAPH
+SELECT count_paper/count_tot_paper, selected.yr, selected.mn 
+    FROM (
+        SELECT count(p.paper_id) AS count_paper, year(dt_created) AS yr, month(dt_created) AS mn 
+            FROM papers p 
+            INNER JOIN active_papers ap 
+                ON p.paper_id = ap.paper_id 
+                GROUP BY yr, mn
+        ) selected 
+    INNER JOIN (
+        SELECT count(paper_id) AS count_tot_paper, year(dt_created) AS yr, month(dt_created) AS mn 
+            FROM papers 
+            GROUP BY year(dt_created), month(dt_created)) total 
+    ON selected.yr = total.yr AND selected.mn = total.mn;
 
 
+-- SUBJECT GRAPH
+SELECT count(subject_name) AS count_sub, subject_name 
+    FROM subjects s 
+    INNER JOIN active_papers ap 
+        ON ap.paper_id = s.paper_id 
+    GROUP BY s.subject_name
+    ORDER BY count_sub DESC;
 
 
-$query = "SELECT count(paper_id) AS count_paper,                ".
-"       year(dt_created) AS yr,                        ".
-"       month(dt_created) AS mn                        ".
-"       FROM papers p                                    ".
-"       INNER JOIN active_papers ap".
-"        ON p.paper_id = ap.paper_id".
-"       GROUP BY year(dt_created), month(dt_created); ";
+-- AUTHOR COLLOBORATION
+SELECT a1.author_name, a2.author_name 
+    FROM authors a1 
+    INNER JOIN active_papers ap 
+        ON ap.paper_id = a1.paper_id 
+    INNER JOIN authors a2
+        ON a1.paper_id = a2.paper_id AND a1.author_name < a2.author_name;
+
+------------------------------
+-- SANITY CHECKS -------------
+------------------------------
+
+-- -- 1
+-- -- Desc: temporary tables join to form correct query
+-- -- Assertion: The following search should return 1 paper
+
+-- papers temp table
+DROP TABLE IF EXISTS temp_papers;
+CREATE TEMPORARY TABLE temp_papers AS (
+    SELECT p.paper_id
+        FROM papers p
+        WHERE MATCH (p.title, p.description) AGAINST ('Seiberg-Witten' IN BOOLEAN MODE)
+);
+
+-- authors temp table
+DROP TABLE IF EXISTS temp_authors;
+CREATE TEMPORARY TABLE temp_authors AS (
+SELECT a.paper_id
+    FROM authors a
+    WHERE MATCH (a.author_name) AGAINST ('Trimm' IN BOOLEAN MODE)
+    GROUP BY a.paper_id
+    HAVING count(a.paper_id) = 1
+);
+
+-- subjects table
+DROP TABLE IF EXISTS temp_subjects;
+CREATE TEMPORARY TABLE temp_subjects AS (
+SELECT s.paper_id
+    FROM subjects s
+    WHERE MATCH (s.subject_name) AGAINST ('HighEnergyPhysics-Theory' IN BOOLEAN MODE)
+    GROUP BY s.paper_id
+    HAVING count(s.paper_id) = 1
+);
+
+SELECT count(tp.paper_id)
+    FROM temp_papers tp
+    INNER JOIN temp_authors ta 
+    ON ta.paper_id = tp.paper_id
+    INNER JOIN temp_subjects ts 
+    ON ts.paper_id = tp.paper_id;
 
 
-       WHERE paper_id IN(                             
-            SELECT paper_id                           
-                 FROM active_papers                   
-        )                                             
-        GROUP BY year(dt_created), month(dt_created); 
+-- -- 2
+-- -- Test: Author Collaboration returns the correct number of 
+-- -- Assertion: should return 6 edges
+-- -- 
+-- -- NOTE: If we are going to weight each author, if there are n authors present, we should assign a weight of 1/(n-1) for each author for each paper
+-- --       This way, having more authors doesn't accidentally increase the weight of each author in that graph
+-- -- 
 
+DROP TABLE IF EXISTS temp_papers;
+CREATE TEMPORARY TABLE temp_papers AS (
+    SELECT p.paper_id
+        FROM papers p
+        WHERE paper_id = "oai:arXiv.org:0704.0109"
+);
 
-$query = "  select count_paper/count_tot_paper, A.yr, A.mn from                                          ".                                          
-        "  (select count(paper_id) as count_paper, year(dt_created) as yr, month(dt_created) as mn      ".
-        "    from papers                                                                                ".
-        "    where paper_id in(                                                                         ".
-        "    select paper_id                                                                            ".
-        "    from active_papers)                                                                        ".
-        "    group by year(dt_created), month(dt_created)) A join (                                     ".
-        "    select count(paper_id) as count_tot_paper, year(dt_created) as yr, month(dt_created) as mn ".
-        "    from papers                                                                                ".
-        "    group by year(dt_created), month(dt_created)) B                                            ".
-        "  where A.yr = B.yr and A.mn = B.mn;                                                           ";
+-- combine temporary tables into an active table set
+DROP TABLE IF EXISTS active_papers;
+CREATE TEMPORARY TABLE active_papers AS (
+SELECT tp.paper_id
+    FROM temp_papers tp
+    INNER JOIN authors ta 
+    ON ta.paper_id = tp.paper_id
+    INNER JOIN subjects ts 
+    ON ts.paper_id = tp.paper_id
+);
 
-
-$query = " select count(subject_name) as count_sub, subject_name ".
-" from subjects                                         ".
-" inner join active_papers                              ".
-" on active_papers.paper_id = subjects.paper_id         ".
-" group by subject_name                                 ".
-" order by count_sub desc;                              ";
-
------------------------------------------------------------------------------------------------
------------------------------------------------------------------------------------------------
------------------------------------------------------------------------------------------------
------------------------------------------------------------------------------------------------
------------------------------------------------------------------------------------------------
------------------------------------------------------------------------------------------------
-
-" -- papers                                                                                  ".
-" DROP TABLE IF EXISTS temp_papers;                                                          ".
-" CREATE TEMPORARY TABLE temp_papers AS (                                                    ".
-"     SELECT p.paper_id                                                                      ".
-"         FROM papers p                                                                      ".
-"         WHERE MATCH (p.title, p.description) AGAINST (".$query_keywords." IN BOOLEAN MODE) ".
-" );                                                                                         ".
-"                                                                                            ".
-" -- authors                                                                                 ".
-" DROP TABLE IF EXISTS temp_authors;                                                         ".
-" CREATE TEMPORARY TABLE temp_authors AS (                                                   ".
-" SELECT a.paper_id                                                                          ".
-"     FROM authors a                                                                         ".
-"     WHERE MATCH (a.author_name) AGAINST (".$query_authors." IN BOOLEAN MODE)               ".
-"     GROUP BY a.paper_id                                                                    ".
-"     HAVING count(a.paper_id) = 1                                                           ".
-" );                                                                                         ".
-"                                                                                            ".
-" -- subjects                                                                                ".
-" DROP TABLE IF EXISTS temp_subjects;                                                        ".
-" CREATE TEMPORARY TABLE temp_subjects AS (                                                  ".
-" SELECT s.paper_id                                                                          ".
-"     FROM subjects s                                                                        ".
-"     WHERE MATCH (s.subject_name) AGAINST (".$query_subjects." IN BOOLEAN MODE)             ".
-"     GROUP BY s.paper_id                                                                    ".
-"     HAVING count(s.paper_id) = 1                                                           ".
-" );                                                                                         ".
-"                                                                                            ".
-"                                                                                            ".
-" SELECT tp.paper_id                                                                         ".
-"     FROM temp_papers tp                                                                    ".
-"     INNER JOIN temp_authors ta                                                             ".
-"     ON ta.paper_id = tp.paper_id                                                           ".
-"     INNER JOIN temp_subjects ts                                                            ".
-"     ON ts.paper_id = tp.paper_id;                                                          ";
-
+-- AUTHOR COLLOBORATION
+SELECT a1.author_name, a2.author_name 
+    FROM authors a1 
+    INNER JOIN ( SELECT DISTINCT paper_id FROM active_papers) ap 
+        ON ap.paper_id = a1.paper_id 
+    INNER JOIN authors a2
+        ON a1.paper_id = a2.paper_id AND a1.author_name < a2.author_name;
