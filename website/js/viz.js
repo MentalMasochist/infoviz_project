5801a31b5c7ced54ad01a12a6c1e692a7c8791a9
Richard Brooks <richardbrks@gmail.com>
2014-11-12 10:18:27 -0500

    line graph added with static data

--- website/js/viz.js
+++ website/js/viz.js
@@ -66,23 +66,67 @@
       // extra_viz(data); // to be competed in latter stages
     };
 
-    function trend_viz(data) {
+    function trend_viz() {
 
-      console.log(data);
+      var margin = {top: 20, right: 20, bottom: 30, left: 50},
+          width = 1000 - margin.left - margin.right,
+          height = 270 - margin.top - margin.bottom;
 
-      var sampleSVG = d3.select("#viz_trend")
-          .append("svg")
-          .attr("width", 100)
-          .attr("height", 100);    
+      var parseDate = d3.time.format("%d-%b-%y").parse;
 
-      sampleSVG.append("circle")
-          .style("stroke", "gray")
-          .style("fill", "white")
-          .attr("r", 40)
-          .attr("cx", 50)
-          .attr("cy", 50)
-          .on("mouseover", function(){d3.select(this).style("fill", "grey");})
-          .on("mouseout", function(){d3.select(this).style("fill", "white");});
+      var x = d3.time.scale()
+          .range([0, width]);
+
+      var y = d3.scale.linear()
+          .range([height, 0]);
+
+      var xAxis = d3.svg.axis()
+          .scale(x)
+          .orient("bottom");
+
+      var yAxis = d3.svg.axis()
+          .scale(y)
+          .orient("left");
+
+      var line = d3.svg.line()
+          .x(function(d) { return x(d.date); })
+          .y(function(d) { return y(d.close); });
+
+      var svg = d3.select("#viz_trend").append("svg")
+          .attr("width", width + margin.left + margin.right)
+          .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+          .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+      d3.tsv("./data/trend.tsv", function(error, data) {
+        data.forEach(function(d) {
+          d.date = parseDate(d.date);
+          d.close = +d.close;
+        });
+
+        x.domain(d3.extent(data, function(d) { return d.date; }));
+        y.domain(d3.extent(data, function(d) { return d.close; }));
+
+        svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis);
+
+        svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis)
+          .append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 6)
+            .attr("dy", ".71em")
+            .style("text-anchor", "end")
+            .text("Price ($)");
+
+        svg.append("path")
+            .datum(data)
+            .attr("class", "line")
+            .attr("d", line);
+      });
     };
 
     function subject_network_viz(data) {
@@ -102,22 +146,7 @@
           .on("mouseout", function(){d3.select(this).style("fill", "white");});
     };
 
-    function author_network_viz(data) {
-      console.log(data)
-      var sampleSVG = d3.select("#viz_graph_author")
-          .append("svg")
-          .attr("width", 100)
-          .attr("height", 100);    
-
-      sampleSVG.append("circle")
-          .style("stroke", "gray")
-          .style("fill", "white")
-          .attr("r", 40)
-          .attr("cx", 50)
-          .attr("cy", 50)
-          .on("mouseover", function(){d3.select(this).style("fill", "grey");})
-          .on("mouseout", function(){d3.select(this).style("fill", "white");});
-    };
+    function author_network_viz() { };
 
     function word_cloud_viz(data) {
       var sampleSVG = d3.select("#viz_word_cloud")
@@ -135,18 +164,4 @@
           .on("mouseout", function(){d3.select(this).style("fill", "white");});
     };
 
-    function extra_viz(data) {
-      var sampleSVG = d3.select("#extra")
-          .append("svg")
-          .attr("width", 100)
-          .attr("height", 100);    
-
-      sampleSVG.append("circle")
-          .style("stroke", "gray")
-          .style("fill", "white")
-          .attr("r", 40)
-          .attr("cx", 50)
-          .attr("cy", 50)
-          .on("mouseover", function(){d3.select(this).style("fill", "grey");})
-          .on("mouseout", function(){d3.select(this).style("fill", "white");});
-    };
+    function extra_viz() {  };
