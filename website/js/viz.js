bbb940d1598456686843efcf9225d70e71faa485
Richard Brooks <richardbrks@gmail.com>
2014-11-18 18:22:55 -0500

    static author network graph working

--- website/js/viz.js
+++ website/js/viz.js
@@ -60,85 +60,86 @@
   
     function main_viz(response) {
       trend_viz(response['trending_data']);
-      subject_network_viz(response['subject_data']);
       author_network_viz(response['author_data']);
+      // subject_network_viz(response['subject_data']);
       // word_cloud_viz(data);  // to be competed in latter stages
       // extra_viz(data); // to be competed in latter stages
     };
 
     function trend_viz(response) {
 
-      // console.log("trend viz");
-      console.log(typeof response);
+      // // console.log("trend viz");
+      // console.log(typeof response);
 
-      var margin = {top: 20, right: 50, bottom: 30, left: 70},
-          width = 1125 - margin.left - margin.right,
-          height = 270 - margin.top - margin.bottom;
+      // var margin = {top: 20, right: 50, bottom: 30, left: 70},
+      //     width = 1125 - margin.left - margin.right,
+      //     height = 270 - margin.top - margin.bottom;
 
-      // var parseDate = d3.time.format("%d-%b-%y").parse;
-      var parseDate = d3.time.format("%Y-%d").parse;
+      // // var parseDate = d3.time.format("%d-%b-%y").parse;
+      // var parseDate = d3.time.format("%Y-%d").parse;
 
-      var x = d3.time.scale()
-          .range([0, width]);
+      // var x = d3.time.scale()
+      //     .range([0, width]);
 
-      var y = d3.scale.linear()
-          .range([height, 0]);
+      // var y = d3.scale.linear()
+      //     .range([height, 0]);
 
-      var xAxis = d3.svg.axis()
-          .scale(x)
-          .orient("bottom");
+      // var xAxis = d3.svg.axis()
+      //     .scale(x)
+      //     .orient("bottom");
 
-      var yAxis = d3.svg.axis()
-          .scale(y)
-          .orient("left");
+      // var yAxis = d3.svg.axis()
+      //     .scale(y)
+      //     .orient("left");
 
-      var line = d3.svg.line()
-          .x(function(d) { return x(d.date); })
-          .y(function(d) { return y(d.freq); });
+      // var line = d3.svg.line()
+      //     .x(function(d) { return x(d.date); })
+      //     .y(function(d) { return y(d.freq); });
 
-      var svg = d3.select("#viz_trend").append("svg")
-          .attr("width", width + margin.left + margin.right)
-          .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-          .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+      // var svg = d3.select("#viz_trend").append("svg")
+      //     .attr("width", width + margin.left + margin.right)
+      //     .attr("height", height + margin.top + margin.bottom)
+      //   .append("g")
+      //     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-      // d3.tsv("./data/trend.tsv", function(error, data) {
-      //   data.forEach(function(d) {
+      // // d3.tsv("./data/trend.tsv", function(error, data) {
+      // //   data.forEach(function(d) {
+      // //     d.date = parseDate(d.date);
+      // //     d.freq = +d.freq;
+      // //   });
+
+      //   root = response;
+      //   root.forEach(function(d) {
       //     d.date = parseDate(d.date);
       //     d.freq = +d.freq;
       //   });
 
-        root = response;
-        root.forEach(function(d) {
-          d.date = parseDate(d.date);
-          d.freq = +d.freq;
-        });
+      //   x.domain(d3.extent(root, function(d) { return d.date; }));
+      //   y.domain(d3.extent(root, function(d) { return d.freq; }));
 
-        x.domain(d3.extent(root, function(d) { return d.date; }));
-        y.domain(d3.extent(root, function(d) { return d.freq; }));
+      //   svg.append("g")
+      //       .attr("class", "x axis")
+      //       .attr("transform", "translate(0," + height + ")")
+      //       .call(xAxis);
 
-        svg.append("g")
-            .attr("class", "x axis")
-            .attr("transform", "translate(0," + height + ")")
-            .call(xAxis);
+      //   svg.append("g")
+      //       .attr("class", "y axis")
+      //       .call(yAxis)
+      //     .append("text")
+      //       .attr("transform", "rotate(-90)")
+      //       .attr("y", 6)
+      //       .attr("dy", ".71em")
+      //       .style("text-anchor", "end")
+      //       .text("Frequency");
 
-        svg.append("g")
-            .attr("class", "y axis")
-            .call(yAxis)
-          .append("text")
-            .attr("transform", "rotate(-90)")
-            .attr("y", 6)
-            .attr("dy", ".71em")
-            .style("text-anchor", "end")
-            .text("Frequency");
-
-        svg.append("path")
-            .datum(root)
-            .attr("class", "line")
-            .attr("d", line);
+      //   svg.append("path")
+      //       .datum(root)
+      //       .attr("class", "line")
+      //       .attr("d", line);
       };
 
     function subject_network_viz(data) {
+
       var sampleSVG = d3.select("#viz_graph_subject")
           .append("svg")
           .attr("width", 100)
@@ -154,7 +155,101 @@
           .on("mouseout", function(){d3.select(this).style("fill", "white");});
     };
 
-    function author_network_viz() { };
+    function author_network_viz(response) { 
+        var w = 500,
+            h = 334,
+            fill = d3.scale.category20();
+
+        var vis = d3.select("#viz_graph_author")
+          .append("svg:svg")
+            .attr("width", w)
+            .attr("height", h)
+            .attr("transform", "translate(" + -100 + "," + 0 + ")");
+
+        d3.json("cites.json", function(json) {
+          console.log(json);
+
+          var force = d3.layout.force()
+              .charge(-20)
+              .linkDistance(10)
+              .nodes(json.nodes)
+              .links(json.links)
+              .size([w, h])
+              .start();
+
+          var link = vis.selectAll("line.link")
+              .data(json.links)
+              .enter().append("svg:line")
+              .attr("class", "link")
+              .style("stroke-width", function(d) { return Math.sqrt(d.value); })
+              .attr("x1", function(d) { return d.source.x; })
+              .attr("y1", function(d) { return d.source.y; })
+              .attr("x2", function(d) { return d.target.x; })
+              .attr("y2", function(d) { return d.target.y; });
+
+          var node = vis.selectAll("g.node")
+              .data(json.nodes)
+              .enter().append("svg:g")
+              .attr("class", "node")
+
+            node.append("svg:circle")
+              .attr("r", 5)
+                .style("fill", function(d) { return fill(d.group); })
+            .call(force.drag).on("mouseover", fade(.1)).on("mouseout", fade(1));;
+              //.call(force.drag);
+
+         var linkedByIndex = {};
+            json.links.forEach(function(d) {
+                linkedByIndex[d.source.index + "," + d.target.index] = 1;
+            });
+
+            function isConnected(a, b) {
+                return linkedByIndex[a.index + "," + b.index] || linkedByIndex[b.index + "," + a.index] || a.index == b.index;
+            }
+
+            function fade(opacity) {
+                return function(d) {
+                    node.style("stroke-opacity", function(o) {
+                        thisOpacity = isConnected(d, o) ? 1 : opacity;
+                        this.setAttribute('fill-opacity', thisOpacity);
+                        return thisOpacity;
+                    });
+
+                    link.style("stroke-opacity", function(o) {
+                        return o.source === d || o.target === d ? 1 : opacity;
+                    });
+                };
+            }
+            
+          node.append("svg:text")
+            .attr("class", "nodetext")
+            .attr("dx", 10)
+          .attr("dy", ".35em")
+          .text(function(d) { return d.name; });
+
+          node.append("svg:title")
+            .text(function(d) { return d.name; });
+
+          vis.style("opacity", 1e-6)
+            .transition()
+              .duration(1000)
+              .style("opacity", 1);
+
+          force.on("tick", function() {
+            link.attr("x1", function(d) { return d.source.x; })
+                .attr("y1", function(d) { return d.source.y; })
+                .attr("x2", function(d) { return d.target.x; })
+                .attr("y2", function(d) { return d.target.y; });
+            
+            node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+          });
+        });
+
+
+    };
+
+
+
 
     function word_cloud_viz(data) {
       var sampleSVG = d3.select("#viz_word_cloud")
