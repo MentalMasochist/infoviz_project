cfd90f16ab568362ebd751327aeef9ef7989b911
Richard Brooks <richardbrks@gmail.com>
2014-11-18 19:38:35 -0500

    trend and author network(static) both working together

--- website/js/viz.js
+++ website/js/viz.js
@@ -68,74 +68,74 @@
 
     function trend_viz(response) {
 
-      // // console.log("trend viz");
-      // console.log(typeof response);
+      // console.log("trend viz");
+      console.log(typeof response);
 
-      // var margin = {top: 20, right: 50, bottom: 30, left: 70},
-      //     width = 1125 - margin.left - margin.right,
-      //     height = 270 - margin.top - margin.bottom;
+      var margin = {top: 20, right: 50, bottom: 30, left: 70},
+          width = 1125 - margin.left - margin.right,
+          height = 270 - margin.top - margin.bottom;
 
-      // // var parseDate = d3.time.format("%d-%b-%y").parse;
-      // var parseDate = d3.time.format("%Y-%d").parse;
+      // var parseDate = d3.time.format("%d-%b-%y").parse;
+      var parseDate = d3.time.format("%Y-%d").parse;
 
-      // var x = d3.time.scale()
-      //     .range([0, width]);
+      var x = d3.time.scale()
+          .range([0, width]);
 
-      // var y = d3.scale.linear()
-      //     .range([height, 0]);
+      var y = d3.scale.linear()
+          .range([height, 0]);
 
-      // var xAxis = d3.svg.axis()
-      //     .scale(x)
-      //     .orient("bottom");
+      var xAxis = d3.svg.axis()
+          .scale(x)
+          .orient("bottom");
 
-      // var yAxis = d3.svg.axis()
-      //     .scale(y)
-      //     .orient("left");
+      var yAxis = d3.svg.axis()
+          .scale(y)
+          .orient("left");
 
-      // var line = d3.svg.line()
-      //     .x(function(d) { return x(d.date); })
-      //     .y(function(d) { return y(d.freq); });
+      var line = d3.svg.line()
+          .x(function(d) { return x(d.date); })
+          .y(function(d) { return y(d.freq); });
 
-      // var svg = d3.select("#viz_trend").append("svg")
-      //     .attr("width", width + margin.left + margin.right)
-      //     .attr("height", height + margin.top + margin.bottom)
-      //   .append("g")
-      //     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+      var svg = d3.select("#viz_trend").append("svg")
+          .attr("width", width + margin.left + margin.right)
+          .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+          .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-      // // d3.tsv("./data/trend.tsv", function(error, data) {
-      // //   data.forEach(function(d) {
-      // //     d.date = parseDate(d.date);
-      // //     d.freq = +d.freq;
-      // //   });
-
-      //   root = response;
-      //   root.forEach(function(d) {
+      // d3.tsv("./data/trend.tsv", function(error, data) {
+      //   data.forEach(function(d) {
       //     d.date = parseDate(d.date);
       //     d.freq = +d.freq;
       //   });
 
-      //   x.domain(d3.extent(root, function(d) { return d.date; }));
-      //   y.domain(d3.extent(root, function(d) { return d.freq; }));
+        root = response;
+        root.forEach(function(d) {
+          d.date = parseDate(d.date);
+          d.freq = +d.freq;
+        });
 
-      //   svg.append("g")
-      //       .attr("class", "x axis")
-      //       .attr("transform", "translate(0," + height + ")")
-      //       .call(xAxis);
+        x.domain(d3.extent(root, function(d) { return d.date; }));
+        y.domain(d3.extent(root, function(d) { return d.freq; }));
 
-      //   svg.append("g")
-      //       .attr("class", "y axis")
-      //       .call(yAxis)
-      //     .append("text")
-      //       .attr("transform", "rotate(-90)")
-      //       .attr("y", 6)
-      //       .attr("dy", ".71em")
-      //       .style("text-anchor", "end")
-      //       .text("Frequency");
+        svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis);
 
-      //   svg.append("path")
-      //       .datum(root)
-      //       .attr("class", "line")
-      //       .attr("d", line);
+        svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis)
+          .append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 6)
+            .attr("dy", ".71em")
+            .style("text-anchor", "end")
+            .text("Frequency");
+
+        svg.append("path")
+            .datum(root)
+            .attr("class", "line")
+            .attr("d", line);
       };
 
     function subject_network_viz(data) {
